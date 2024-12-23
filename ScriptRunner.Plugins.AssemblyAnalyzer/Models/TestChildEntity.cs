78b79dc84ef83bf41c42c3564fc6666200ab786d
Peter van de Pas <peter.vdpas@protonmail.com>
2024-12-23 01:18:37 +0100

    demo child and parent

---
+++ ScriptRunner.Plugins.AssemblyAnalyzer/Models/TestChildEntity.cs
@@ -0,0 +1,35 @@
+﻿namespace ScriptRunner.Plugins.AssemblyAnalyzer.Models;
+
+/// <summary>
+/// Represents a child entity in a hierarchical structure.
+/// </summary>
+public class TestChildEntity
+{
+    /// <summary>
+    /// The unique identifier for the child entity.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The name of the child entity.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The foreign key linking this child to its parent.
+    /// </summary>
+    public int ParentEntityId { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestChildEntity"/> class with values.
+    /// </summary>
+    /// <param name="id">The child's unique identifier.</param>
+    /// <param name="name">The child's name.</param>
+    /// <param name="parentEntityId">The ID of the parent entity this child is linked to.</param>
+    public TestChildEntity(int id, string name, int parentEntityId)
+    {
+        Id = id;
+        Name = name;
+        ParentEntityId = parentEntityId;
+    }
+}
---
+++ ScriptRunner.Plugins.AssemblyAnalyzer/Models/TestParentEntity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace ScriptRunner.Plugins.AssemblyAnalyzer.Models;
+
+/// <summary>
+/// Represents a parent entity in a hierarchical structure.
+/// </summary>
+public class TestParentEntity
+{
+    /// <summary>
+    /// The unique identifier for the parent entity.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The name of the parent entity.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// A collection of child entities linked to this parent.
+    /// </summary>
+    public List<TestChildEntity> Children { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TestParentEntity"/> class with values.
+    /// </summary>
+    /// <param name="id">The parent's unique identifier.</param>
+    /// <param name="name">The parent's name.</param>
+    /// <param name="children">The children linked to this parent.</param>
+    public TestParentEntity(int id, string name, List<TestChildEntity>? children)
+    {
+        Id = id;
+        Name = name;
+        Children = children ?? [];
+    }
+}
